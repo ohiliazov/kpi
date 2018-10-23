c5e58d0377a79c7bac337e1fdee7433d8e57d510
Oleksandr Hiliazov <oleksandr.hiliazov@gmail.com>
2018-10-24 00:28:42 +0300

    Додав калькулятор

---
+++ programming/calculator.py
@@ -0,0 +1,119 @@
+"""
+Курсова робота
+
+Тема: Калькулятор.
+За заданим символьним рядком з математичною формулою обчислити значення
+функції в заданій точці (з використанням польського інверсного запису)
+
+Виконав: Гілязов Олександр Ігорович
+Група: ІС-зп81
+"""
+
+import operator
+
+# Тут задаються символи, які калькулятор буде розпізнавати
+ops = {
+    '+': operator.add,
+    '-': operator.sub,
+    '−': operator.sub,
+    '*': operator.mul,
+    '×': operator.mul,
+    '/': operator.truediv,
+    '//': operator.floordiv,
+    '%': operator.mod,
+    '^': operator.pow,
+}
+
+
+class BadEquationError(Exception):
+    pass
+
+
+def calculate(math_equation: str):
+    stack = []
+
+    for s in math_equation.split():
+        if s in ops:
+
+            if len(stack) < 2:
+                raise BadEquationError("Not enough numbers.")
+
+            a, b = stack.pop(), stack.pop()
+
+            operation = ops[s](b, a)
+
+            stack.append(operation)
+        else:
+            try:
+                stack.append(int(s))
+            except ValueError:
+                raise BadEquationError("Invalid character: %s" % s)
+
+    if len(stack) > 1:
+        raise BadEquationError("Too many numbers in stack left: %s " % stack)
+
+    result = stack.pop()
+
+    print("%s is %s" % (math_equation, result))
+    return result
+
+
+def test_valid():
+    valid_strings = {
+        "1 1 +": 2,
+        "2 3 -": -1,
+        "4 5 *": 20,
+        "5 2 /": 2.5,
+        "7 2 //": 3,
+        "9 2 ^": 81,
+        "11 12 13 + *": 275,
+        "1 2 + 4 × 5 + 3 −": 14
+    }
+
+    for k, v in valid_strings.items():
+        assert calculate(k) == v
+
+
+def test_invalid():
+    invalid_strings = [
+        "- 1",
+        "+ 2",
+        "* 3",
+        "/ 4 5",
+        "^ 6",
+        "7 8",
+        "9 10 + -",
+        "11 12 * /",
+        "13 14 &",
+        "a b c + +"
+    ]
+
+    for s in invalid_strings:
+        try:
+            calculate(s)
+
+        except BadEquationError as ex:
+            print("Exception raised: %s" % ex)
+            pass
+
+        else:
+            raise AssertionError("Test failed on: %s" % s)
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            equation = input("Enter equation RPN: ")
+
+            if equation in ["", "quit", "exit", "close"]:
+                break
+
+            elif equation == 'test':
+                test_valid()
+                test_invalid()
+            else:
+                calculate(equation)
+
+        except BadEquationError as e:
+            print(e)
+            pass
