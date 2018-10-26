c33a5be0cbd8fbdff097dab9d7b9d6363ae02c6c
Oleksandr Hiliazov <oleksandr.hiliazov@gmail.com>
2018-10-26 22:21:30 +0300

    Added test func

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -14,6 +14,3 @@ def test_insertion_sort():
     assert insertion_sort([1, 2, 3, 4, 5, 6, 7, 8]) == [1, 2, 3, 4, 5, 6, 7, 8]
     assert insertion_sort([8, 7, 6, 5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5, 6, 7, 8]
     assert insertion_sort([3, 7, 4, 8, 1, 5, 2, 6]) == [1, 2, 3, 4, 5, 6, 7, 8]
-
-
-test_insertion_sort()
--- algorithms/merge_sort.py
+++ algorithms/merge_sort.py
@@ -40,6 +40,3 @@ def test_merge_sort():
     assert merge_sort([1, 2, 3, 4, 5, 6, 7, 8]) == [1, 2, 3, 4, 5, 6, 7, 8]
     assert merge_sort([8, 7, 6, 5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5, 6, 7, 8]
     assert merge_sort([3, 7, 4, 8, 1, 5, 2, 6]) == [1, 2, 3, 4, 5, 6, 7, 8]
-
-
-test_merge_sort()
