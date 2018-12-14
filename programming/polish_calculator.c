2ec97d8f39e809f10030ccb28074fa8eba76bca7
Gelya <oleksandr.hiliazov@gmail.com>
2018-12-14 13:46:48 +0200

    Delete polish_calculator.c

--- programming/polish_calculator.c
+++
@@ -1,9 +0,0 @@
-void main(int argc, char *argv[])
-{
-    int i = 0;
-    printf("argc = %d\n", argc);
-    
-    for (i = 0; i < argc; i++) {
-        printf("argv[%d] = %s\n", i, argv[i]);
-    }
-}
