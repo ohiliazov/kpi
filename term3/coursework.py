afbdb5293a6e6b99667f39f68a221eb5ecbda439
Oleksandr Hiliazov <oleksandr.hiliazov@gmail.com>
2020-03-10 08:54:04 +0200

    Added coursework

--- term3/coursework.py
+++ term3/coursework.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from pulp import *
-
+from pprint import pprint
+import math
 print("===============================================================================================")
 print("Вихідна задача з чіткими умовами")
 print("===============================================================================================")
@@ -33,9 +34,6 @@ def solve_phase_one(consumers: int, producers: int,
     if len(powers) != producers:
         errors.append("Потужність заводів вказана невірно")
 
-    if sum(needs) > sum(powers):
-        errors.append("Попиту споживачів перевищує потужність заводів")
-
     if errors:
         return errors
 
@@ -51,15 +49,15 @@ def solve_phase_one(consumers: int, producers: int,
 
     print("Цільова функція:", transition_cost)
 
-    print("Обмеження за попитом:")
+    # print("Обмеження за попитом:")
     # Обмеження за попитом
     for idx, need in enumerate(needs):
         # print(sum(X[producers*idx:producers*(idx+1)]) == need)
         problem += sum(X[producers*idx:producers*(idx+1)]) == need, f"споживач {idx + 1}"
 
-    print("Обмеження за потужністю:")
+    # print("Обмеження за потужністю:")
     for idx, power in enumerate(powers):
-        print(sum(X[idx::consumers]) <= power)
+        # print(sum(X[idx::consumers]) <= power)
         problem += sum(X[idx::consumers]) <= power, f"завод {idx + 1}"
 
     problem.solve()
@@ -141,68 +139,140 @@ print("+-------------+------------------------------------+------------+--------
       "+-------------+------------------------------------+------------+-----------+---------+----------+")
 
 
-def calc_cost(X, C, W, B_min, B_max, S, R):
-    res = 0
+def solve_phase_two(m: int, n: int, C: list, X: list, B_min, B_max, A: list):
+    Y = [LpVariable(f"Y{i+1}{j+1}", lowBound=0) for i in range(m) for j in range(n)]
+    # for idx, x in enumerate(X):
+    #     Y[idx].setInitialValue(value(x))
+    transition_cost = sum([c * (y - x) for c, y, x in zip(C, Y, X)])
+    problem = pulp.LpProblem("0", LpMinimize)
+    problem += transition_cost, "Цільова функція"
 
-    for c, x in zip(C, X):
-        res += value(c * x)
-    print(res)
-    for j in range(4):
-        res += R[j] * ((B_max[j] + B_min[j]) / 2 - W[j])
-        res += ((S[j] + R[j]) / (2 * (B_max[j] - B_min[j]))) * ((W[j] - B_min[j]) ** 2)
+    # Обмеження за попитом
+    # for idx, (b_min, b_max) in enumerate(zip(B_min, B_max)):
+    #     problem += sum(Y[n * idx:n * (idx + 1)]) >= b_min, f"B{idx + 1}1"
+    #     problem += sum(Y[n * idx:n * (idx + 1)]) <= b_max, f"B{idx + 1}2"
 
-    return res
+    # Обмеження за потужністю
+    for idx, a in enumerate(A):
+        problem += sum(Y[idx::m]) == a, f"A{idx + 1}1"
+
+    problem += sum(Y) == sum(X), f"total"
+    problem.solve()
+
+    return problem
 
 
-def gradients(m, n, C, W, A, B_min, B_max, S, R):
+def calc_cost(m, n, X, C, B_min, B_max, S, Q):
+    trans_cost = value(sum([c * x for c, x in zip(C, X)]))
+
+    extra_cost = 0
+    for j in range(n):
+        b = (B_min[j] + B_max[j]) / 2
+        w = value(sum(X[n*j:n*j+m]))
+
+        q = Q[j]
+        s = S[j]
+        b_min = B_min[j]
+        b_max = B_max[j]
+        extra_cost += q * (b - w) + 0.5 * (s + q) * (w - b_min) ** 2 / (b_max - b_min)
+
+    return trans_cost, extra_cost
+
+
+def gradients(m, n, C, X, B_min, B_max, S, Q):
     grads = []
 
     for i in range(m):
         for j in range(n):
-            grad = C[i*n+j] - R[j] + (S[j] + R[j]) / (B_max[j] - B_min[j]) * (W[j] - B_min[j])
-            grads.append(grad)
+            c = C[i*n+j]
+            q = Q[j]
+            s = S[j]
+            b_min = B_min[j]
+            b_max = B_max[j]
+            # print(c, q, s, b_min, b_max)
 
+            w = sum(X[n*j:n*j+m])
+
+            grad = c - q + (s + q) * (w - b_min) / (b_max - b_min)
+            grads.append(grad)
+    print(grads)
     return grads
 
 
+def calc_alpha(m, n, C, X, Y, B_min, B_max, S, Q):
+    lambda1 = 0
+    lambda2 = 0
+    for c, x, y in zip(C, X, Y):
+        lambda1 += c * (y - x)
+
+    for j in range(n):
+        s = S[j]
+        q = Q[j]
+        b_min = B_min[j]
+        b_max = B_max[j]
+
+        X_j = X[n * j:n * j + m]
+        Y_j = Y[n * j:n * j + m]
+        coeff = 0.5 * (s + q) / (b_max - b_min)
+
+        diff = sum([y - x for x, y in zip(X_j, Y_j)])
+
+        lambda1 += coeff * 2 * diff * (sum(X_j) - b_min) - q * diff
+        lambda2 += coeff * diff ** 2
+
+    if lambda2 == 0:
+        return 0
+
+    return -0.5 * lambda1 / lambda2
+
+
+def diff(X, Y):
+    return math.sqrt(sum([(value(y) - value(x))**2 for x, y in zip(X, Y)]))
+
+
+def solve_with_gradients(m, n, X, C, A, B_min, B_max, S, Q):
+    X_hist = []
+    F_hist = []
+    X = [value(x) for x in X]
+    cost = calc_cost(m, n, X, C, B_min, B_max, S, Q)
+    print('initial vars:', X)
+    print('initial cost:', cost)
+    i = 0
+    for i in range(10):
+        X_hist.append([x for x in X])
+        F_hist.append(cost)
+        i += 1
+        grads = gradients(m, n, C, X, B_min, B_max, S, Q)
+        prob = solve_phase_two(m, n, grads, X, B_min, B_max, A)
+        new_X = [value(x) for x in prob.variables()]
+        print(new_X)
+        l = max(-1, min(1, calc_alpha(m, n, C, X, new_X, B_min, B_max, S, Q)))
+
+        if abs(l) < 0.0001:
+            break
+
+        X = [x + round(l*(value(new_x) - x)) for new_x, x in zip(new_X, X)]
+        cost = calc_cost(m, n, X, C, B_min, B_max, S, Q)
+        print('iter', i, 'vars:', X)
+        print('iter', i, 'cost:', cost)
+
+    print("END")
+    print([sum(X[i*n:i*n+m]) for i in range(m)])
+    for x, f in zip(X_hist, F_hist):
+        print(x, f, sum(f))
+    return X_hist, F_hist
+
+
 C = [5, 3, 6, 2, 3, 4, 5, 6, 7, 5, 6, 4, 4, 8, 5, 3]
+C2 = [9, 5, 6, 0]
+C2 = [0, 2, 0, 0]
+C = [c+c2 for c, c2 in zip(C, itertools.cycle(C2))]
+
 X = vars2.variables()
 A = [500, 1100, 600, 0]
-W = [400, 800, 200, 800]
 B_min = [300, 700, 200, 600]
 B_max = [500, 1100, 400, 1000]
-B = [(b_max+b_min)/2 for b_max, b_min in zip(B_max, B_min)]
-R = [5, 6, 6, 3]
+Q = [5, 6, 600, 3]
 S = [8, 10, 19, 6]
 
-X_grads = gradients(4, 4, C, W, A, B_min, B_max, S, R)
-Y = []
-for i in range(4):
-    for j in range(4):
-        Y.append(pulp.LpVariable(f"Y{i + 1}{j + 1}", lowBound=0, cat=pulp.LpInteger))
-print(X_grads)
-cost = sum([x0*y for x0, y in zip(X_grads, Y)])
-print(cost)
-prob_y = LpProblem('grad1', LpMinimize)
-prob_y += cost, 'grad_cost'
-
-print("Обмеження за попитом:")
-# Обмеження за попитом
-for idx, need in enumerate(B):
-    print(sum(Y[4*idx:4*(idx+1)]) == need)
-    prob_y += sum(Y[4 * idx:4 * (idx + 1)]) == need, f"споживач {idx + 1}"
-
-print("Обмеження за потужністю:")
-for idx, power in enumerate(A):
-    print(sum(Y[idx::4]) <= power)
-    prob_y += sum(Y[idx::4]) <= power, f"завод {idx + 1}"
-
-prob_y.solve()
-
-print("План поставки:")
-for variable in prob_y.variables():
-    if variable.varValue > 0:
-        var_name = variable.name
-        print(f"Завод {var_name[2]} -> Споживач {var_name[1]}: {int(variable.varValue)}")
-
-print(f"Витрати на транспортування: {int(value(cost))}")
+solve_with_gradients(4, 4, X, C, A, B_min, B_max, S, Q)
